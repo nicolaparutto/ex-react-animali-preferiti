311b01c9d0bbe3d8bd10d8a6cf162b1745d07e34
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-07 16:47:05 +0200

    feat: milestone 3 completed

--- AnimalsComponent.jsx
+++ AnimalsComponent.jsx
@@ -2,26 +2,42 @@ const animalsChoices = ["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"];
 const { useState } = React;
 
 function AnimalsComponent() {
+
 	const [animals, setAnimals] = useState([]);
+	const [show, setShow] = useState(false);
+	const [animalAdded, setAnimalAdded] = useState('');
 
 	const addAnimal = () => {
-		const numRandom = Math.floor(Math.random() * 5);
-		const animalToAdd = animalsChoices[numRandom];
-		console.log(animalToAdd);
-		if (animals.includes(animalToAdd)) {
-			alert(`
-				Non ho inserito nessun animale, perche 
-				la lista conteneva già l'animale generato 
-				casualmente: ${animalToAdd}
-				`)
-		} else {
-			setAnimals([...animals, animalToAdd])
-		}
+		setAnimals(curr => [...animals, animalAdded]);
+		setAnimalAdded('');
+		setShow(false)
+	}
+
+	function Modal({
+		title,
+		content,
+		show = false,
+		onClose = () => { },
+		onConfirm = () => { }
+	}) {
+		return show && ReactDOM.createPortal(
+			<div className="modal-container">
+				<div className="modal">
+					<h2>{title}</h2>
+					{content}
+					<div>
+						<button onClick={onClose}>Annulla</button>
+						<button onClick={onConfirm}>Conferma</button>
+					</div>
+				</div>
+			</div>,
+			document.body
+		)
 	}
 
 	return (
 		<>
-			<button onClick={addAnimal}>Aggiungi animale</button>
+			<button onClick={() => setShow(true)}>Aggiungi animale</button>
 			<details>
 				<summary>
 					<span>Animali</span>
@@ -36,22 +52,28 @@ function AnimalsComponent() {
 					}
 				</ul>
 			</details>
+			<Modal
+				show={show}
+				onClose={() => setShow(false)}
+				onConfirm={addAnimal}
+				title="Aggiungi un animale"
+				content={
+					<input type="text" value={animalAdded} onChange={e => setAnimalAdded(e.target.value)} />
+				}
+			/>
 		</>
 	)
 }
 
+
+
+
 // Importo la funzione createRoot:
 const { createRoot } = ReactDOM;
-
 // Seleziono l'elemento dal DOM in cui voglio inserire il componente:
 const AnimalsList = document.querySelector(".lista-animali");
-
 // Salvo l'oggetto restituito dalla funzione createRoot, a cui passo come parametro, 
 // l'elemento del DOM interessato all'inserimento del componente:
 const root = createRoot(AnimalsList);
-
 // Tramite il metodo render, passo il componente all'elemento del DOM:
 root.render(<AnimalsComponent />)
-
-
-
